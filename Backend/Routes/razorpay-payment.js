f1684f3d96dc6ea159dbe2117aada2c7ca191207
GAURAV KUMAR <gauravrajpoot452@gmail.com>
2023-02-13 22:26:05 +0530

    Done payment integration current state of app is -: click on buy button -> pops payment card of razorpay prebuilt -> sending all the data to backend creating order from backend -> verifying after  successfull payment.

--- Backend/Routes/razorpay-payment.js
+++ Backend/Routes/razorpay-payment.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const crypto = require("crypto");
 const Razorpay = require('razorpay');
 const instance = new Razorpay({
     key_id: process.env.KEY_ID,
@@ -10,14 +11,30 @@ const instance = new Razorpay({
 app.post('/', (req, res) => {
     const{price} = req.body;
     const options = {
-        amount: Number(price),  // amount in the smallest currency unit
+        amount: Number(price)*100,  // amount in the smallest currency unit
         currency: "INR",
       };
     instance.orders.create(options, function(err, order) {
-        console.log(order);
+        if(err){
+            return res.send({code : 404, message : "server error"});
+        }
+        return res.send({code:200, message : "order created" , data : order, keyId : process.env.KEY_ID});
     });
 })
 
-module.exports = app;
+app.post('/verify', (req, res) => {
+    let body=req.body.razorpay_order_id + "|" + req.body.razorpay_payment_id;
+    
+    let expectedSignature = crypto.createHmac('sha256', process.env.KEY_SECRET).update(body.toString()).digest('hex');
+    // let response = {"signatureIsValid":"false"}
+    if(expectedSignature === req.body.razorpay_signature){
+        // response={"signatureIsValid":"true"}
+        res.send({code : 200, message : 'Payment Verified'});
+    }
+    else{
+        res.send({code : 404, message : 'Payment Not Verified'});
+    }
+})
 
+module.exports = app;
   
--- Frontend/src/Components/Product-Details-Page/product-detail.js
+++ Frontend/src/Components/Product-Details-Page/product-detail.js
@@ -16,40 +16,80 @@ function ProductDetail() {
         },
         body: JSON.stringify({ _id })
       })
-      .then(res => res.json())
-      .then(data => {
-        setProduct(data);
-        console.log(data);
-      })
-      .catch(err => console.log(err));
-      }
-      fetchData();
-    },[])
-    
-    async function AddtoCart(){
-      await fetch('http://localhost:5000/addToCart', {
-        method : 'POST',
-        headers : {
-          'Content-Type' : 'application/json'
-        },
-        body : JSON.stringify({
-          _id
+        .then(res => res.json())
+        .then(data => {
+          setProduct(data);
+          // console.log(data);
         })
-      })
+        .catch(err => console.log(err));
     }
+    fetchData();
+  }, [])
 
-    async function payment(){
-      const{price} = product;
-      await fetch('http://localhost:5000/payment', {
-        method : 'POST',
-        headers : {
-          'Content-Type' : 'application/json'
-        },
-        body : JSON.stringify({
-          price
-        })
+  async function AddtoCart() {
+    await fetch('http://localhost:5000/addToCart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        _id
       })
-    }
+    })
+  }
+
+  function OpenPaymentPage(Data) {
+    let options = {
+      key: Data.keyId,
+      amount: Data.data.amount,
+      currency: Data.data.currency,
+      name: "Company name",
+      order_id: Data.data.id,
+      handler: function (response) {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = response;
+        fetch('http://localhost:5000/payment/verify', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            razorpay_order_id, razorpay_payment_id, razorpay_signature
+          })
+        })
+        .then(res => res.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      }
+    };
+    var rzp1 = new window.Razorpay(options);
+    rzp1.open();
+    console.log(Data);
+  }
+
+  async function payment() {
+    const { price } = product;
+    await fetch('http://localhost:5000/payment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        price
+      })
+    })
+      .then(res => res.json())
+      .then((data) => {
+        // console.log(data);
+        OpenPaymentPage(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 
   return (
     <Container className="product-detail">
@@ -62,9 +102,7 @@ function ProductDetail() {
           <Card.Body>
             <Card.Title>{product.title}</Card.Title>
             <Card.Text>Price : {product.price}</Card.Text>
-            <Link to='/payment'>
-              <Button type="submit" className="btn btn-lg" onClick={payment}>Buy Now</Button>
-            </Link>
+            <Button type="submit" className="btn btn-lg" onClick={payment}>Buy Now</Button>
             <Button type="submit" className="btn btn-lg" onClick={AddtoCart}>Add to Cart</Button>
           </Card.Body>
         </Col>
